2d7d42e60b55a1560eb8d352e0b085640ede01b1
WXHRR <1147232219@qq.com>
2023-06-14 15:09:55 +0800

    update

--- src/views/header/ChatHeader.vue
+++ src/views/header/ChatHeader.vue
@@ -5,7 +5,7 @@ import { ref } from "vue";
 const props = defineProps({
   allPeoples: {
     type: Number,
-    default: 0
+    default: 0,
   },
 });
 
@@ -13,7 +13,17 @@ const open = ref(false);
 </script>
 <template>
   <div class="chat-header">
-    <div>相亲相爱一家人({{ allPeoples }})</div>
+    <div>
+      相亲相爱一家人({{ allPeoples }})
+      <span
+        style="
+          color: #3e4452;
+          font-size: 12px;
+          transform: scale(0.8) translate(-5px, 4px);
+        "
+        >2.0.1</span
+      >
+    </div>
     <div class="icon" @click="open = true">
       <el-icon size="20">
         <More />
